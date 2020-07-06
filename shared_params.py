651b452f5b9bf632cc9db0347dd7ad0d182912f8
xzackli <xzackli@gmail.com>
2020-07-06 19:55:09 -0400

    comparing the fiducials and shared params

---
+++ shared_params.py
@@ -0,0 +1,23 @@
+shared_params = {    
+    'output': 'tCl,pCl,lCl,mPk',
+    'l_max_scalars': 5000,
+    'lensing': 'yes',
+    'non linear': '',
+    'gauge' : 'synchronous',
+    'A_s': 2.3e-9,
+    'n_s': 0.96, 
+    'h': 0.67,
+    'omega_b': 0.022,
+    'omega_cdm': 1e-12,
+    'omega_dmeff': 0.12,
+    'm_dmeff':1,
+    
+    # precision params
+    'reionization_optical_depth_tol': 1e-07,
+    'tol_Tdmeff_integration': 1e-8,
+    'tol_background_integration': 1e-8,
+    'tol_perturb_integration': 1e-8,
+    'tol_thermo_integration': 1e-8,
+    'perturb_sampling_stepsize':0.01,
+    'k_max_tau0_over_l_max' : 6
+}
